7648fbc3b7f2e580d63c96504904ac61804fa333
SchoolTechBytes <nicholas.j.nixon.27@outlook.com>
2026-07-25 11:37:01 -0500

    Set up server.js, wired up entry point and started the routes (#46)

---
+++ server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import router from './routes/routes.js';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+/**
+  * Routes
+  */
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+});
